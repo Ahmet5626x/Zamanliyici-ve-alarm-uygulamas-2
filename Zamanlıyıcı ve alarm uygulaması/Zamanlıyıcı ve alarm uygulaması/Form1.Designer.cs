8e1c5cac929be4b52ff95fd6b672bd4434b29c3c
Ahmet ŞEN <144745009+Ahmet5626x@users.noreply.github.com>
2024-12-20 12:48:42 +0300

    Add files via upload

---
+++ Zamanlıyıcı ve alarm uygulaması/Zamanlıyıcı ve alarm uygulaması/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Zamanlıyıcı_ve_alarm_uygulaması
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Tasarımcı desteği için gerekli metot
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.txtTimeInput = new System.Windows.Forms.TextBox();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AlarmZamanı = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TekrarAralığı = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SesliAlarm = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAlarmEkle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAlarmTimeInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnBaslat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnBaslat.Location = new System.Drawing.Point(86, 135);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 36);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = false;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.BackColor = System.Drawing.Color.Crimson;
+            this.btnDurdur.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnDurdur.Location = new System.Drawing.Point(177, 135);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(75, 36);
+            this.btnDurdur.TabIndex = 2;
+            this.btnDurdur.Text = "Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = false;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.BackColor = System.Drawing.Color.Cyan;
+            this.btnSifirla.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSifirla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSifirla.Location = new System.Drawing.Point(125, 191);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(75, 36);
+            this.btnSifirla.TabIndex = 3;
+            this.btnSifirla.Text = "Sıfırla";
+            this.btnSifirla.UseVisualStyleBackColor = false;
+            this.btnSifirla.Click += new System.EventHandler(this.btnSifirla_Click);
+            // 
+            // txtTimeInput
+            // 
+            this.txtTimeInput.Location = new System.Drawing.Point(98, 55);
+            this.txtTimeInput.Name = "txtTimeInput";
+            this.txtTimeInput.Size = new System.Drawing.Size(144, 22);
+            this.txtTimeInput.TabIndex = 4;
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblCountdown.Location = new System.Drawing.Point(93, 80);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(149, 39);
+            this.lblCountdown.TabIndex = 5;
+            this.lblCountdown.Text = "00:00:00";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(329, 155);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.AlarmZamanı,
+            this.TekrarAralığı,
+            this.SesliAlarm});
+            this.dataGridView1.Location = new System.Drawing.Point(224, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(430, 150);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // AlarmZamanı
+            // 
+            this.AlarmZamanı.HeaderText = "Alarm Zamanı";
+            this.AlarmZamanı.MinimumWidth = 6;
+            this.AlarmZamanı.Name = "AlarmZamanı";
+            this.AlarmZamanı.Width = 125;
+            // 
+            // TekrarAralığı
+            // 
+            this.TekrarAralığı.HeaderText = "Tekrar Aralığı";
+            this.TekrarAralığı.MinimumWidth = 6;
+            this.TekrarAralığı.Name = "TekrarAralığı";
+            this.TekrarAralığı.Width = 125;
+            // 
+            // SesliAlarm
+            // 
+            this.SesliAlarm.HeaderText = "Sesli Alarm";
+            this.SesliAlarm.MinimumWidth = 6;
+            this.SesliAlarm.Name = "SesliAlarm";
+            this.SesliAlarm.Width = 125;
+            // 
+            // btnAlarmEkle
+            // 
+            this.btnAlarmEkle.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAlarmEkle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAlarmEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnAlarmEkle.Location = new System.Drawing.Point(376, 237);
+            this.btnAlarmEkle.Name = "btnAlarmEkle";
+            this.btnAlarmEkle.Size = new System.Drawing.Size(96, 32);
+            this.btnAlarmEkle.TabIndex = 9;
+            this.btnAlarmEkle.Text = "Alarm Ekle";
+            this.btnAlarmEkle.UseVisualStyleBackColor = false;
+            this.btnAlarmEkle.Click += new System.EventHandler(this.btnAlarmEkle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(121, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Zaman girin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(372, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Alarm kurun:";
+            // 
+            // txtAlarmTimeInput
+            // 
+            this.txtAlarmTimeInput.Location = new System.Drawing.Point(354, 203);
+            this.txtAlarmTimeInput.Name = "txtAlarmTimeInput";
+            this.txtAlarmTimeInput.Size = new System.Drawing.Size(144, 22);
+            this.txtAlarmTimeInput.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAlarmEkle);
+            this.Controls.Add(this.txtAlarmTimeInput);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.txtTimeInput);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Zamanlayıcı ve Alarm Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnSifirla;
+        private System.Windows.Forms.TextBox txtTimeInput;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AlarmZamanı;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TekrarAralığı;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SesliAlarm;
+        private System.Windows.Forms.Button btnAlarmEkle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAlarmTimeInput;
+    }
+}
---
+++ Zamanlıyıcı ve alarm uygulaması/Zamanlıyıcı ve alarm uygulaması/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Zamanlıyıcı_ve_alarm_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        private int countdownTime = 0;
+        private Timer timer = new Timer { Interval = 1000 };
+        private int alarmTime = -1;
+        private SoundPlayer soundPlayer;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Tick += Timer_Tick;
+
+            // SoundPlayer nesnesini örneklendir
+            soundPlayer = new SoundPlayer(@"C:\ses\3.wav");
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Geri sayım çalışıyorsa
+            if (countdownTime > 0)
+            {
+                countdownTime--;
+                lblCountdown.Text = TimeSpan.FromSeconds(countdownTime).ToString(@"hh\:mm\:ss");
+            }
+
+            // Şu anki yerel saati al
+            DateTime now = DateTime.Now;
+
+            // Alarm zamanı kontrolü
+            if (alarmTime > 0 && DateTime.TryParse(txtAlarmTimeInput.Text, out DateTime alarmDateTime))
+            {
+                // Eğer şu anki saat alarm saatine eşitse alarmı çalıştır
+                if (now >= alarmDateTime)
+                {
+                    timer.Stop(); // Zamanlayıcıyı durdur
+                    PlayAlarmSound(); // Alarm sesini çal
+                    MessageBox.Show("Alarm zamanı geldi!", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Alarmı temizle (aynı alarm tekrar çalmasın)
+                    alarmTime = -1;
+                }
+            }
+
+            // Geri sayım sıfıra ulaştıysa
+            if (countdownTime == 0)
+            {
+                timer.Stop();
+                PlayAlarmSound();
+                MessageBox.Show("Zaman doldu!", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        private void PlayAlarmSound()
+        {
+            try
+            {
+                soundPlayer.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ses çalma sırasında bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtTimeInput.Text, out int timeInSeconds))
+            {
+                countdownTime = timeInSeconds;
+                lblCountdown.Text = TimeSpan.FromSeconds(countdownTime).ToString(@"hh\:mm\:ss");
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli bir zaman girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            countdownTime = 0;
+            lblCountdown.Text = "00:00:00";
+        }
+
+        private void btnAlarmEkle_Click(object sender, EventArgs e)
+        {
+            // DateTimePicker'dan seçilen tarihi al
+            DateTime selectedDateTime = dateTimePicker1.Value;
+
+            // Seçilen tarihi txtAlarmTimeInput'a yaz
+            txtAlarmTimeInput.Text = selectedDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            // Alarm zamanı kontrolü
+            if (selectedDateTime > DateTime.Now)
+            {
+                alarmTime = (int)(selectedDateTime - DateTime.Now).TotalSeconds;
+
+                // Alarm verisini DataGridView'a ekle
+                string sesliAlarm = "Evet"; // Varsayılan olarak sesli alarm ekleniyor
+                dataGridView1.Rows.Add(selectedDateTime.ToString("yyyy-MM-dd HH:mm:ss"), selectedDateTime.ToString("HH:mm:ss"), sesliAlarm);
+
+                MessageBox.Show("Alarm verisi eklendi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Alarm zamanı geçmiş bir tarih olamaz.", "Geçersiz Zaman", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblCountdown.Text = "00:00:00";
+
+            // DateTimePicker'ın formatını ayarla
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+
+            // DateTimePicker değerini yerel saate ayarla
+            dateTimePicker1.Value = DateTime.Now;
+
+            // Varsayılan olarak yerel saati txtAlarmTimeInput'a yaz
+            txtAlarmTimeInput.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
